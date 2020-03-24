6b8fff7428dbd86daf40ab66f738da95a6adcc3c
oriondev7 <57668339+oriondev7@users.noreply.github.com>
2020-03-24 17:15:46 +0200

    Add files via upload

---
+++ upper.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+char * upper(char * str);
+
+int main() {
+    char str[] = "This is C++";
+    std::cout << upper(str) << std::endl;
+    return 0;
+}
+
+char * upper(char * str)
+{
+    int size = 0;
+    while(str[size] != '\0')
+        size++;
+    str[++size - 1] = '\0';
+    for(int i = 0; i < size; i++)
+        if(str[i] >= 97 && str[i] <= 122)
+            str[i] -= 32;
+    return str;
+}
